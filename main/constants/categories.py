6f5fb86c850d24bfe3dd9b8cc721f1a938faa88d
andrew-medrano <amedrano.az@gmail.com>
2024-11-22 17:44:02 -0500

    icons for filters added

--- main/constants/categories.py
+++ main/constants/categories.py
@@ -2,3 +2,14 @@ CATEGORIES = ["Communications", "Materials", "Software",
             "Medical", "Military", "Energy", "Electronics", 
             "Sensors", "Environmental" ]
 
+category_icon_paths = {
+    "Communications": "main/static/communications.svg",
+    "Materials": "main/static/materials.svg",
+    "Software": "main/static/software.svg",
+    "Medical": "main/static/medical.svg",
+    "Military": "main/static/military.svg",
+    "Energy": "main/static/energy.svg",
+    "Electronics": "main/static/electronics.svg",
+    "Sensors": "main/static/sensors.svg",
+    "Environmental": "main/static/environmental.svg"
+}
